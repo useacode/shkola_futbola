2db5df50a4fcf2bc47de48c657313bd436e7fe79
useacode <strettos@gmail.com>
2016-10-17 00:28:27 +0300

    scroll fix

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
    $('a[href^="#"]').click(function () { 
      elementClick = $(this).attr("href");
      destination = $(elementClick).offset().top;
-     $('body').animate( { scrollTop: destination }, 1100 );
+     $('body,html').animate( { 'scrollTop': destination }, 1100 );
      return false;
    });
  });
